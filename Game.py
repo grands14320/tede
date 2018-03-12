a9593fcb0d40e95a1e16c0a76d10ebdfdd7cfcb1
janek <grands14320@gmail.com>
2018-03-12 22:36:18 +0100

    halo halo mietku

---
+++ Game.py
@@ -0,0 +1,16 @@
+import pygame
+import os.path
+
+# sprobowac z try i expect z importowaniem
+
+# test
+# file_directory_list = []
+# for file in file_directory_list:
+#     if os.path.isfile(str(file)):
+#         pass
+#     else:
+#         print("no file " + str(file) + " found")
+
+
+class Game:
+    pass
