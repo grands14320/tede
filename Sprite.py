34f703d936692d8a63192df54c4e33164b4391a0
10kw10 <kw10leo@interia.pl>
2018-03-13 12:26:05 +0100

    Create Sprite.py

---
+++ Sprite.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Sprite:
+    def __init__(self, size=(0,0), position=(0,0), color = (0,0,0)):
+        self.sprite = pygame.Surface(size)
+        self.sprite.fill(color)
+        self.position = position
+        self.size = size
+        self.rotation = 0
+        self.color = color
+        self.origin = (0,0)
+
+    def move(self, offset = (0,0)):
+        self.position = tuple(map(lambda x, y: x + y, self.position, offset))
+
+    def rotate(self, angle = 0):
+        self.sprite = pygame.Surface(self.size)
+        self.sprite.fill(self.color)
+        self.sprite.set_colorkey((0,0,0))
+        self.rotation += angle
+        self.rotation %= 360
+        self.sprite = pygame.transform.rotate(self.sprite, self.rotation)
+        self.origin = self.sprite.get_rect()
+        self.origin.center = self.position
+
+    def draw(self, window):
+        window.blit(self.sprite, self.origin)
