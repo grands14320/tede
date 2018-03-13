841c51f330d7b2148f7124cede0ab9bc40fcb960
10kw10 <kw10leo@interia.pl>
2018-03-13 12:34:20 +0100

    Update Sprite.py

--- Sprite.py
+++ Sprite.py
@@ -1,27 +1,34 @@
 import pygame
 
 class Sprite:
-    def __init__(self, size=(0,0), position=(0,0), color = (0,0,0)):
+    def __init__(self, size=(0, 0), position=(0, 0), color=(0, 0, 0)):
         self.sprite = pygame.Surface(size)
         self.sprite.fill(color)
         self.position = position
         self.size = size
         self.rotation = 0
         self.color = color
-        self.origin = (0,0)
+        self.origin = (0, 0)
 
-    def move(self, offset = (0,0)):
+    def setPosition(self, newPosition=(0, 0)):
+        self.position = newPosition
+
+    def move(self, offset=(0, 0)):
         self.position = tuple(map(lambda x, y: x + y, self.position, offset))
 
-    def rotate(self, angle = 0):
+    def setRotation(self, newRotation=0):
+        self.rotation = newRotation
         self.sprite = pygame.Surface(self.size)
         self.sprite.fill(self.color)
-        self.sprite.set_colorkey((0,0,0))
-        self.rotation += angle
-        self.rotation %= 360
+        self.sprite.set_colorkey((0, 0, 0))
         self.sprite = pygame.transform.rotate(self.sprite, self.rotation)
         self.origin = self.sprite.get_rect()
         self.origin.center = self.position
+
+    def rotate(self, angle=0):
+        self.rotation += angle
+        self.rotation %= 360
+        self.setRotation(self.rotation)
 
     def draw(self, window):
         window.blit(self.sprite, self.origin)
