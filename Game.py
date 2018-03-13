598ca7a1718e0d675cced2ea5d6f1aa1af5aea4f
janek <grands14320@gmail.com>
2018-03-13 15:33:07 +0100

    check if file exists and prints it to the choosed window

--- Game.py
+++ Game.py
@@ -1,16 +1,19 @@
 import pygame
 import os.path
 
-# sprobowac z try i expect z importowaniem
-#
-# test
-file_directory_list = []
-for file in file_directory_list:
-    if os.path.isfile(str(file)):
-        pass
-    else:
-        print("no file " + str(file) + " found")
+pygame.init()
 
 
 class Game:
-    pass
+    red = (255, 0, 0)
+
+    # w sumie to nie wiem czy to tu
+    def text_render(self, window, text_to_render, color=red, size=20):
+        my_font = pygame.font.SysFont('Comic Sans MS', size)
+        rendered = my_font.render(text_to_render, 0, color)
+        window.blit(rendered, (100, 100))
+
+    def check_files(self, file_name_list, window):
+        errors = [file for file in file_name_list if os.path.isfile('images/' + file)]
+        Game.text_render(window, errors)
+
