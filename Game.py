4e31cfe8ff7b1285f041af41f8cc3564f0061dcd
janek <grands14320@gmail.com>
2018-03-13 15:35:54 +0100

    check if file exists and prints it to the choosed window

--- Game.py
+++ Game.py
@@ -8,10 +8,10 @@ class Game:
     red = (255, 0, 0)
 
     # w sumie to nie wiem czy to tu
-    def text_render(self, window, text_to_render, color=red, size=20):
+    def text_render(self, window, text_to_render, destination=(100, 100), color=red, size=20):
         my_font = pygame.font.SysFont('Comic Sans MS', size)
         rendered = my_font.render(text_to_render, 0, color)
-        window.blit(rendered, (100, 100))
+        window.blit(rendered, destination)
 
     def check_files(self, file_name_list, window):
         errors = [file for file in file_name_list if os.path.isfile('images/' + file)]
