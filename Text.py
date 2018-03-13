9774560abceee0a93618af4b06e3c5794486259d
10kw10 <kw10leo@interia.pl>
2018-03-13 21:55:46 +0100

    Update Text.py

--- Text.py
+++ Text.py
@@ -4,16 +4,14 @@ pygame.init()
 
 
 class Text:
-    red = (255, 0, 0)
 
-    def __init__(self, string, position, color=red, font_size=30):
+    def __init__(self, string, position, color=(255, 0, 0), font_size=30):
         self.text = str(string)
         self.color = color
         self.position = position
         self.font_size = font_size
 
-    def text_render(self):
+    def draw(self, window):
         my_font = pygame.font.SysFont('Comic Sans MS', self.font_size)
-        rendered = my_font.render(self.text, 0, self.color)
-        return rendered
+        window.blit(my_font.render(self.text, 0, self.color), self.position)
 
