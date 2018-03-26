45f1fc7a282c70ac6092573c1db8a40667f7d493
janek <grands14320@gmail.com>
2018-03-26 17:27:45 +0200

    adjust

--- Tile_manager.py
+++ Tile_manager.py
@@ -4,12 +4,10 @@ import Sprite
 class TileManager:
     def __init__(self):
         self.sprite = Sprite.Sprite((60, 60))
+        self.tiles = [self.sprite.set_fill_color((10, 159, 85)),  # light green
+                      self.sprite.set_fill_color((10, 124, 48)),  # dark green
+                      self.sprite.set_fill_color((103, 110, 110))  # path
+                      ]
 
     def get_tile(self, point):
-        tiles = [self.sprite.set_fill_color((10, 159, 85)),  # light green
-                 self.sprite.set_fill_color((10, 124, 48)),  # dark green
-                 self.sprite.set_fill_color((103, 110, 110))  # path
-                 ]
-        return tiles[point]
-
-
+        return self.tiles[point]
