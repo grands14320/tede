c4cca233fe888cbeba9920661d64c30d7a2e76a1
janek <grands14320@gmail.com>
2018-03-26 18:56:53 +0200

    .

--- Tile_manager.py
+++ Tile_manager.py
@@ -4,11 +4,9 @@ import Sprite
 class TileManager:
     def __init__(self):
         self.sprite = Sprite.Sprite((60, 60))
-        self.tiles = [self.sprite.set_fill_color((10, 159, 85)),  # light green
-                      self.sprite.set_fill_color((10, 124, 48)),  # dark green
-                      self.sprite.set_fill_color((103, 110, 110))  # path
-                      ]
+        self.tiles = [self.sprite.sprite,
+                      self.sprite.sprite,
+                      self.sprite.sprite]
 
     def get_tile(self, point):
-        print(self.tiles[0])
-        return self.tiles[int(point)]
+        return self.tiles[int(point) - 1]
